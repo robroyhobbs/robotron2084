de7d203e22edc534ae240971db9b62684fff342e
robroyhobbs <41485426+robroyhobbs@users.noreply.github.com>
2025-03-15 18:10:32 -0700

    Add simple test file

---
+++ build/web/assets/main.py
@@ -0,0 +1,22 @@
+import pygame
+import asyncio
+
+async def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill((0, 0, 0))
+        pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(350, 250, 100, 100))
+        pygame.display.flip()
+        
+        clock.tick(60)
+        await asyncio.sleep(0)
+
+asyncio.run(main()) 
